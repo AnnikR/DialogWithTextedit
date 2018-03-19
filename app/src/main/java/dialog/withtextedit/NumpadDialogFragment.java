51d6ab4c0e38e5a2792c8161f62d96a2f0d7f136
Annika Ronkainen <t5roan00@students.oamk.fi>
2018-03-19 10:40:12 +0200

    Ohjelma tarkistaa onko TextEdit vastaus oikein vai väärin sekä tunnistaa, jos "cancel" painiketta on painettu.

--- app/src/main/java/dialog/withtextedit/NumpadDialogFragment.java
+++ app/src/main/java/dialog/withtextedit/NumpadDialogFragment.java
@@ -29,11 +29,11 @@ public class NumpadDialogFragment extends DialogFragment {
     ChoiceListener AnswerListener;
 
     public interface ChoiceListener {
-        void onDialogPositiveClick(DialogFragment dialog);
+//        void onDialogPositiveClick(DialogFragment dialog);
         void onDialogNegativeClick(DialogFragment dialog);
 
-//        void onDialogNumberClickCorrect(DialogFragment dialog);
-//        void onDialogNumberClickWrong(DialogFragment dialog);
+        void onDialogNumberClickCorrect(DialogFragment dialog);
+        void onDialogNumberClickWrong(DialogFragment dialog);
     }
 
     @Override
@@ -54,9 +54,9 @@ public class NumpadDialogFragment extends DialogFragment {
         int number2 = r.nextInt(5-1)+ 1;
 
         String questionOnScreen = "Laske: " + String.valueOf(number1) + " + " + String.valueOf(number2);
-        final Integer answer = number1 + number2;
+        final Integer correctAnswer = number1 + number2;
 
-//        final String numberToEdittext;
+        final Integer[] userAnswer = new Integer[1];
 
         final LinearLayout layout = new LinearLayout(getActivity());
         layout.setOrientation(LinearLayout.VERTICAL);
@@ -64,8 +64,9 @@ public class NumpadDialogFragment extends DialogFragment {
         //uusi AlertDialog
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
 
+        //TextEdit vastauksen näyttämistä varten
         final EditText typedAnswerText = new EditText(getActivity());
-        typedAnswerText.setHint("Vastaus");
+        typedAnswerText.setHint("Vastaus: ");
         typedAnswerText.setInputType(InputType.TYPE_NULL);
         layout.addView(typedAnswerText);
 
@@ -80,36 +81,39 @@ public class NumpadDialogFragment extends DialogFragment {
         numberGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-//                typedAnswerText.setText((Integer) numberGridView.getItemAtPosition(position));
                 String selectedItem = parent.getItemAtPosition(position).toString();
-                typedAnswerText.setText("Vastaus : " + selectedItem);
+                userAnswer[0] = Integer.parseInt(selectedItem);
+                typedAnswerText.setText("Vastaus : " + userAnswer[0]);
             }
         });
         layout.addView(numberGridView);
-
-//        typedAnswerText.setInputType(InputType.TYPE_CLASS_NUMBER);
 
         dialogBuilder.setPositiveButton("OK",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         //OK button clicked
-//                        String answerString = answerText.getText().toString();
-                        AnswerListener.onDialogPositiveClick(NumpadDialogFragment.this);
+
+                        if(userAnswer[0] == correctAnswer)
+                        {
+                            AnswerListener.onDialogNumberClickCorrect(NumpadDialogFragment.this);
+                        }
+                        else
+                        {
+                            AnswerListener.onDialogNumberClickWrong(NumpadDialogFragment.this);
+                        }
                     }
                 });
         dialogBuilder.setNegativeButton("Cancel",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         //Cancel button clicked
+
                         AnswerListener.onDialogNegativeClick(NumpadDialogFragment.this);
                     }
                 });
 
         //määritä AlertDialogin otsikoksi kysymys, ja otsikon alle näkymäksi numeroruudukko.
         dialogBuilder.setTitle(questionOnScreen);
-//        dialogBuilder.setTitle("%s + s% = ", number1, number2);
-//        dialogBuilder.setView(typedAnswerText);
         dialogBuilder.setView(layout);
         return dialogBuilder.create();
     }
